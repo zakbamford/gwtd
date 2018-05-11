7efd2772449d3a3ec422ac46c3f01b9b261174e5
zakbamford <zakbamford@gmail.com>
2018-05-11 10:50:12 -0700

    updates

--- GridWorld Tower Defense/src/WavePart.java
+++ GridWorld Tower Defense/src/WavePart.java
@@ -1,6 +1,6 @@
 public class WavePart {
 
-	private Enemy enemyType; // the type of enemy in this wave part
+	//private Enemy enemyType; // the type of enemy in this wave part
 	private int count; // the amount of enemies to be spawn
 	private int ticksBetweenSpawn; // the enemy spawn rate
 	private int startTime; // how far into the wave this wave part should start
@@ -8,9 +8,9 @@ public class WavePart {
 										// started spawning
 
 	// Constructs a part of a wave with only one enemy type
-	public WavePart(Enemy e, int c, int t, int s) {
+	//public WavePart(Enemy e, int c, int t, int s) {
 
-	}
+	//}
 
 	// gets start time of this wave
 	public int getStartTime() {
