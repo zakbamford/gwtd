2caf2159880e9c96ae913b11399fd5cb77a35b21
zakbamford <zakbamford@gmail.com>
2018-05-11 10:48:27 -0700

    Updated wave classes

--- GridWorld Tower Defense/src/Wave.java
+++ GridWorld Tower Defense/src/Wave.java
@@ -1,4 +1,25 @@
+import java.util.ArrayList;
 
 public class Wave {
 
+	private ArrayList<WavePart> parts;
+	private int ticks;
+
+	// Constructs a wave of enemies consisting of different wave parts
+	public Wave(ArrayList<WavePart> p) {
+		parts = p;
+	}
+
+	// Spawns this wave
+	// precondition: subwaves are sorted by spawn time
+	// postcondition: entire wave has been spawned
+	public void spawn() {
+		for (int i = 0; i < parts.size(); i++) {
+			if (ticks == parts.get(i).getStartTime()) {
+				parts.get(i).spawn();
+				parts.remove(i);
+				i--;
+			}
+		}
+	}
 }
---
+++ GridWorld Tower Defense/src/WavePart.java
@@ -0,0 +1,24 @@
+public class WavePart {
+
+	private Enemy enemyType; // the type of enemy in this wave part
+	private int count; // the amount of enemies to be spawn
+	private int ticksBetweenSpawn; // the enemy spawn rate
+	private int startTime; // how far into the wave this wave part should start
+	private boolean startedSpawning; // whether this wave part has already
+										// started spawning
+
+	// Constructs a part of a wave with only one enemy type
+	public WavePart(Enemy e, int c, int t, int s) {
+
+	}
+
+	// gets start time of this wave
+	public int getStartTime() {
+		return startTime;
+	}
+	
+	// spawns wave
+	public void spawn() {
+		//can't do this yet, needs other classes
+	}
+}
